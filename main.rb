f413557c7855084ea5b7163c215a71d4c0b5bdf3
dooleykh <dooleykh@rose-hulman.edu>
2012-09-20 20:08:07 -0400

    Applied better Ruby style

--- main.rb
+++ main.rb
@@ -1,13 +1,25 @@
 load "WordSearch.rb"
 
-print "Input the file path contianing the puzzle: "
-puts
-#puzzle_file = gets.chomp
-puzzle_file = "./Testing/puzzle.txt"
-print "Input the file path contianing the word list: "
-#word_list_file= gets.chomp
-word_list_file = "./Testing/wordlist.txt"
-ws = WordSearch.new(puzzle_file, word_list_file)
+verbose = false
+if ARGF.argv.size >= 2
+  puzzle_file = ARGF.argv[0]
+  word_list_file = ARGF.argv[1]
+  if ARGF.argv.include?("-v")
+    verbose = true
+  end
+else
+  print "Input the file path contianing the puzzle: "
+  puzzle_file = gets.chomp
+  #  puzzle_file = "./Testing/puzzle.txt"
+  print "Input the file path contianing the word list: "
+  word_list_file= gets.chomp
+  #  word_list_file = "./Testing/wordlist.txt"
+end
 
+if !(File.file?(puzzle_file) && File.file?(word_list_file))
+  raise "One or both files can't be read"
+end
+
+ws = WordSearch.new(puzzle_file, word_list_file, verbose)
 ws.search
 ws.print_puzzle
